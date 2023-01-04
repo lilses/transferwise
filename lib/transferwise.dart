f309411cabe62db585c84f5217d62ff5eab99653
evergreen-trading-systems <4870868+evergreen-trading-systems@users.noreply.github.com>
2023-01-04 18:57:33 +0000

    refactor: regen gen

--- lib/transferwise.dart
+++ lib/transferwise.dart
@@ -3,6 +3,7 @@ library transferwise;
 export 'src/transferwise_statement.api.dart';
 export 'src/transferwise_statement.model.dart';
 export 'src/transferwise_payment.model.dart';
+export 'src/transferwise_payment.api.dart';
 
 
 
