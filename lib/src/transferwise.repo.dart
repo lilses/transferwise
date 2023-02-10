738957b5e67a45e3459bae5666f7639e239769ab
evergreen-trading-systems <4870868+evergreen-trading-systems@users.noreply.github.com>
2023-02-10 17:47:31 +0000

    feat: migrate payment to cart

--- lib/src/transferwise.repo.dart
+++ lib/src/transferwise.repo.dart
@@ -1,8 +1,19 @@
 import 'dart:async';
 
+import 'package:list_component/list_component.dart';
+
 
 class TransferwiseRepo {
+  final int listItemId = 1;
 
   final transferwiseTimer = Stream.periodic(const Duration(seconds: 10), (x) => x).asBroadcastStream();
+
+
+  ListItem toListItem() => ListItem(
+    text1: "Transferwise bank transfer",
+    id: listItemId,
+    text2: "+1.5%",
+    image1: "https://goody-hams.fra1.digitaloceanspaces.com/cryptoworldapi/transferwise.png",
+  );
 
 }
